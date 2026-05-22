9fa4b25ae85d5226f6198cc1701658585d990f28
Pipe1425 <pipezam2005@gmail.com>
2026-05-22 14:36:05 -0500

    FechaNacimiento

--- chistesñeco/src/main/java/co/edu/unbosque/chistesneco/ChistesnecoApplication.java
+++ chistesñeco/src/main/java/co/edu/unbosque/chistesneco/ChistesnecoApplication.java
@@ -1,13 +1,20 @@
 package co.edu.unbosque.chistesneco;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChistesnecoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChistesnecoApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
